88c94c955cf900790de56b87ec868bd270169128
sa46lll <9812smh@naver.com>
2021-09-06 13:08:52 +0900

    [Test] HomeController 테스트

---
+++ src/test/java/tacos/HomeControllerTest.java
@@ -0,0 +1,28 @@
+package tacos;
+
+import static org.hamcrest.CoreMatchers.containsString;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.view;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.test.web.servlet.MockMvc;
+
+@WebMvcTest(HomeController.class) //HomeController의 웹 페이지 테스트
+public class HomeControllerTest {
+	
+	@Autowired
+	private MockMvc mockMvc;
+
+	@Test
+	public void testHomePage() throws Exception {
+		mockMvc.perform(get("/"))  //GET/을 수행한다.
+		.andExpect(status().isOk())  //HTTP 200이 되어야한다.
+		.andExpect(view().name("home"))  //home 뷰가 있어야한다.
+		.andExpect(content().string(containsString("Welcome to...")));  //콘텐츠에 welcome to...가 포함되어야 한다.	
+	}
+
+}
